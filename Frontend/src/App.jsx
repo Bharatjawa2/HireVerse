53d34a5547b164ca84405266d9369628a11140ec
Bharat <jawabharat05@gmail.com>
2025-02-25 12:23:44 +0530

    Shadcn/ui integrated

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Button } from './components/ui/button'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <h1 className='text-2xl text-green-500'>Hello, World!</h1>
+      <Button className="ml-11 text-green-400">Click me</Button>
+    </>
   )
 }
 
