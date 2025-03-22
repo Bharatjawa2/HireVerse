554bc567dc58f0a86ffa364f22e073a12bab45ba
Bharat <jawabharat05@gmail.com>
2025-03-22 17:28:56 +0530

    Deplyed

--- Backend/server.js
+++ Backend/server.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173','https://hireverse.vercel.app'],
     credentials: true
 };
 app.use(cors(corsOptions));
